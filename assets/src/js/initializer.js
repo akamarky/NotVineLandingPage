26b68c0a64c53a6c83bf59e9de6f34feca679cdc
Marko Kovačević <markokovacevic@post.cz>
2018-02-06 12:11:42 +0100

    Started working on it.
    Added responsive navigation bar, boilerplate.

--- assets/src/js/initializer.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-	$('#fullpage').fullpage({autoScrolling: false});
-	loadview("slow");
-});
--- assets/src/js/loadview.js
+++
@@ -1,3 +0,0 @@
-function loadview(speed){
-	$('#app-wrapper').fadeIn(speed).removeClass('hidden');
-}
